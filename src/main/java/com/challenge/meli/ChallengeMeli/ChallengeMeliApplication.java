5bb8aed18038239b1631f8ef33f35ab69afdb276
marcela.chaparro <marcela.chaparro@globant.com>
2022-07-17 23:09:09 -0500

    initial project setup

---
+++ src/main/java/com/challenge/meli/ChallengeMeli/ChallengeMeliApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.meli.ChallengeMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMeliApplication.class, args);
+	}
+
+}
